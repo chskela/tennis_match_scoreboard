55b45e183b9a96fe769e6fe69740231f50c3b633
Chistyakov Alexey <chskela@gmail.com>
2024-06-06 00:25:42 +0300

    feat: added MatchScoreController

---
+++ src/main/kotlin/controllers/MatchScoreController.kt
@@ -0,0 +1,37 @@
+package controllers
+
+import jakarta.servlet.annotation.WebServlet
+import jakarta.servlet.http.HttpServlet
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import models.entities.PlayerInOrder
+import services.MatchScoreCalculationService
+import services.OngoingMatchesService
+import java.util.*
+
+@WebServlet(name = "MatchScoreController", urlPatterns = ["/match-score"])
+class MatchScoreController : HttpServlet() {
+
+    override fun doGet(request: HttpServletRequest, response: HttpServletResponse) {
+        val uuid = UUID.fromString(request.getParameter("uuid"))
+        val currentMatch = OngoingMatchesService.getCurrentMatch(uuid).getOrThrow()
+        request.setAttribute("match", currentMatch)
+        request.setAttribute("uuid", uuid)
+        request.getRequestDispatcher("/templates/match-score.jsp").forward(request, response)
+    }
+
+    override fun doPost(request: HttpServletRequest, response: HttpServletResponse) {
+        val matchScoreCalculationService = MatchScoreCalculationService()
+        val uuid = UUID.fromString(request.getParameter("uuid"))
+        val currentMatch = OngoingMatchesService.getCurrentMatch(uuid).getOrThrow()
+        val winnerPoint = request.getParameter("winner")
+        val updatedMatch = matchScoreCalculationService.calculateMatchScore(currentMatch, handleInput(winnerPoint))
+        OngoingMatchesService.updateMatch(uuid, updatedMatch)
+        response.sendRedirect("/match-score?uuid=$uuid")
+    }
+
+    private fun handleInput(winnerPoint: String): PlayerInOrder = when (winnerPoint) {
+        "First" -> PlayerInOrder.First
+        else -> PlayerInOrder.Second
+    }
+}
